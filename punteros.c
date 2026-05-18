1f28f76f1d4c94a8861d70e311979c7b06fbb7e8
Luis Orozco <luis.orozco.villacis@udla.edu.ec>
2026-05-18 14:09:25 -0500

    Primera version del codigo para punteros - Luis Orozco

---
+++ punteros.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int suma(int a, int b);
+int sumaref(int *ptra, int *ptrb);
+
+int main (int argc, char *argv[]) {
+    int a,b;
+    printf("Ingrese el primer numero: ");
+    scanf("%d",&a);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d",&b);
+    printf("El resultado es: %d", suma(a,b)); //Paso de parametros
+    return 0;
+}
+
+int suma(int a, int b){
+    return a+b;
+}
