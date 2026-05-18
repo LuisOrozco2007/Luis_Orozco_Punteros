02e6c61fc9b0a5adf44b42e9fdf4171be72d9cff
Luis Orozco <luis.orozco.villacis@udla.edu.ec>
2026-05-18 14:15:21 -0500

    Segundo commit del codigo para punteros - Luis Orozco

--- punteros.c
+++ punteros.c
@@ -5,14 +5,19 @@ int sumaref(int *ptra, int *ptrb);
 
 int main (int argc, char *argv[]) {
     int a,b;
+
     printf("Ingrese el primer numero: ");
     scanf("%d",&a);
     printf("Ingrese el segundo numero: ");
     scanf("%d",&b);
-    printf("El resultado es: %d", suma(a,b)); //Paso de parametros
+    printf("El resultado es: %d\n", suma(a,b)); //Paso de parametros
+    printf("El resultado es: %d\n", sumaref(&a,&b));
     return 0;
 }
 
 int suma(int a, int b){
     return a+b;
+}
+int sumaref(int *ptra, int *ptrb){
+    return *ptra + *ptrb;
 }
